5564d84fc2998e03724bf31528f181461b23fa93
Vitaliy Emelyanov <vitaliy030589@gmail.com>
2022-08-07 12:51:52 +0300

    Initial Commit

---
+++ geo-reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geo-reminder
+//
+//  Created by Виталий Емельянов on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
